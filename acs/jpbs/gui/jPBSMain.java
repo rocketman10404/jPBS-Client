22f51a17a907f7352b3f971ec338f40e9d4801ee
Adam Stevens <stevens.adamc@gmail.com>
2013-07-01 23:30:56 -0500

    Initial networking support

--- acs/jpbs/gui/jPBSMain.java
+++ acs/jpbs/gui/jPBSMain.java
@@ -1,5 +1,9 @@
 package acs.jpbs.gui;
 
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 import com.trolltech.qt.core.QRect;
 import com.trolltech.qt.gui.QApplication;
 import com.trolltech.qt.gui.QDesktopWidget;
@@ -8,9 +12,18 @@ import com.trolltech.qt.gui.QLabel;
 import com.trolltech.qt.gui.QPushButton;
 import com.trolltech.qt.gui.QWidget;
 
-public class jPBSMain extends QWidget {
+import acs.jpbs.core.PbsJob;
+import acs.jpbs.core.PbsQueue;
+import acs.jpbs.core.PbsServer;
+import acs.jpbs.net.jPBSClientInterface;
+import acs.jpbs.net.jPBSServerInterface;
+import acs.jpbs.utils.Logger;
+
+public class jPBSMain extends QWidget implements jPBSClientInterface {
 	private final int WIDTH = 500;
 	private final int HEIGHT = 350;
+	private jPBSServerInterface server;
+	private static QLabel console;
 	
 	public jPBSMain() {
 		QDesktopWidget qdw = new QDesktopWidget();
@@ -26,7 +39,16 @@ public class jPBSMain extends QWidget {
 		this.resize(WIDTH, HEIGHT);
 		this.move(x, y);
 		this.setWindowTitle("jPBS Client");
-		this.show();		
+		this.show();
+		
+		Logger.logInfo("Connecting...");
+		try {
+			server = (jPBSServerInterface)Naming.lookup("jPBS-Server");
+			UnicastRemoteObject.exportObject(this);
+			server.register(this);
+		} catch(Exception e) {
+			Logger.logException("Client unable to connect to server.", e);
+		}
 	}
 	
 	private void initGui() {
@@ -36,17 +58,17 @@ public class jPBSMain extends QWidget {
 		// Initialize widgets
 		QPushButton okButton = new QPushButton("Ok", this);
 		QPushButton quitButton = new QPushButton("Quit", this);
-		QLabel consoleWidget = new QLabel("", this);
+		console = new QLabel("", this);
 		
 		// Configure widgets
-		consoleWidget.setStyleSheet("QLabel { background-color: #ffffff; border: 1px solid #000000 }");
+		console.setStyleSheet("QLabel { background-color: #ffffff; border: 1px solid #000000 }");
 		
 		// Bind buttons
 		okButton.clicked.connect(QApplication.instance(), "aboutQt()");
 		quitButton.clicked.connect(QApplication.instance(), "quit()");
 		
 		// Populate layout managers
-		gridMain.addWidget(consoleWidget, 0, 0, 1, 3);
+		gridMain.addWidget(console, 0, 0, 1, 3);
 		gridMain.addWidget(okButton, 1, 1);
 		gridMain.addWidget(quitButton, 1, 2);
 		
@@ -54,6 +76,12 @@ public class jPBSMain extends QWidget {
 		gridMain.setColumnStretch(0, 1);
 		gridMain.setRowStretch(0, 1);
 	}
+	
+	public void updateServer(PbsServer newServer) throws RemoteException { }
+	
+	public void updateQueue(PbsQueue newQueue) throws RemoteException { }
+	
+	public void updateJob(PbsJob newJob) throws RemoteException { }
 	
 	public static void main(String args[]) {
 		QApplication.initialize(args);
